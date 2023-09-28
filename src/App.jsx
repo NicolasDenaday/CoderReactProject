a95408b33cea5a107b89660ab5ca9da216ce6e23
NicolasDenaday <nicodenaday@gmail.com>
2023-09-28 20:58:23 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Saludo from './components/saludo/index'
+import Contador from './components/contador/contador'
+
+function App() {
+
+  return (
+    <>
+    <Contador />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/saludo/index.jsx
@@ -0,0 +1,12 @@
+
+
+const Saludo = ({nombre}) => {
+    return (
+        <button onClick={() => {console.log(nombre)}}>Hacer click</button>
+    )
+}
+
+
+
+
+export default Saludo
